cc357fe32633705da4ec694d498c6715d069dfbc
Mihail Tal <mihail.tal85@gmail.com>
2022-09-03 18:29:31 +0300

    read me updated

--- Camelizes_the_Sentences/camelize.js
+++ Camelizes_the_Sentences/camelize.js
@@ -3,14 +3,21 @@
 // Input : JavaScript-coding-challenge
 // Output : JavaScriptCodingChallenge
 
-
 let Input1 = "JavaScript coding ChAlLenge"
-
 let Input2 = "JavaScript-coding-challenge"
 
 const camel = (str) =>{
-        newStr = str.replaceAll(" ", "").replaceAll("-","")
-        return newStr
+        if (str.match("-")){
+                 newStr = str.split("-")
+        }else {
+                 newStr = str.split(" ")
+        }
+        let newArr = [];
+      for (const x of newStr) {
+        newArr.push(x.charAt(0).toUpperCase()+x.slice(1))
+        }
+        
+        return newArr.join("")
 }
 
 console.log(camel(Input1))
