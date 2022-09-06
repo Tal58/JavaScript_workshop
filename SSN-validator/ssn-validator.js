03fc00c41069f4d6754bb254e18f332e7e5f5342
Mihail Tal <mihail.tal85@gmail.com>
2022-09-06 15:18:45 +0300

    js and image updated

--- SSN-validator/ssn-validator.js
+++ SSN-validator/ssn-validator.js
@@ -2,30 +2,40 @@ const ssnValidator = (str) => {
     let newStr = "";
     let regex = /\d/g
     newStr = str.split("-")
-
-    if ((newStr[0].match(regex).length === 3) && ((newStr[1].match(regex).length === 2)) && ((newStr[2].match(regex).length === 4))) {  ///to check whether the number has 9 digit, seperated by "-"
-        if (parseInt(newStr[0]) < 900) {  //first part should smaller than 900
-            let counter = 0;
-            for (const x of newStr[0]) { ///to check dublicated numbers
-                let s = ""
-                if (x === s) {
-                    counter++
-                } else {
-                    s = x}
+    if ((newStr[0].length === 3) && ((newStr[1].length === 2)) && ((newStr[2].length === 4))) {///to check all length of three part
+        if ((newStr[0].match(regex).length === 3) && ((newStr[1].match(regex).length === 2)) && ((newStr[2].match(regex).length === 4))) {  ///to check whether the number has 9 digit, seperated by "-"
+            if (parseInt(newStr[0]) < 900) {  //first part should smaller than 900
+                let counter = 0;
+                for (const x of newStr[0]) { ///to check dublicated numbers
+                    let s = ""
+                    if (x === s) {
+                        counter++
+                    } else {
+                        s = x
+                    }
                 }
-            if (counter === 3) {
-                console.log("invalid SSN ")
-                return false}
+                if (counter === 3) {
+                    console.log("invalid SSN ")
+                    return false
+                }
+                else {
+                    console.log("valid SSN ")
+                    return true
+                }
+            }
             else {
-                console.log("valid SSN ")
-                return true}
-        }
-        else {
+                console.log("invalid SSN ")
+                return false
+            }
+        } else {
             console.log("invalid SSN ")
-            return false}
-    } else {
+            return false
+        }
+    }
+    else {
         console.log("invalid SSN ")
-        return false}
+        return false
+    }
 }
 console.log(ssnValidator("268-26-3556"))
 console.log(ssnValidator("237-86-7532"))
